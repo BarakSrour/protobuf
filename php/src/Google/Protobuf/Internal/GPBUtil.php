BarakSrour/protobuf

commit f0a3f38321161c50a5907985a4abec9be952d9d8

feat(php): script for updating php reserved words (#27456)
See https://github.com/googleapis/google-cloud-php/issues/9193

`protoc` is generating the invalid class name `Object` when it should be prefixed with `PB` to prevent a conflict with PHP reserved words. I've submitted a fix for this in #27475

This PR introduces the script `php/update_reserved_words.sh` which updates the library in the **8 different classfiles** which all have various ways this logic is defined. Hopefully this makes these things easier for us to maintain in the future.

**NOTE**: prefixing constants with PB has been unnecessary since PHP 7, so we can and should open those up to every word but `class` in the next major version. I've added a comment so we (hopefully) don't forget

```sh
$ bash php/update_reserved_words.sh
Updated src/google/protobuf/compiler/php/names.cc
Updated php/ext/google/protobuf/names.c
Updated src/google/protobuf/compiler/php/php_generator.cc
Updated php/tests/proto/test_reserved_message_lower.proto
Updated php/tests/proto/test_reserved_message_upper.proto
Updated php/tests/proto/test_reserved_enum_lower.proto
Updated php/tests/proto/test_reserved_enum_upper.proto
Updated php/tests/proto/test_reserved_enum_value_lower.proto
Updated php/tests/proto/test_reserved_enum_value_upper.proto
Updated php/tests/GeneratedClassTest.php
Updated php/src/Google/Protobuf/Internal/GPBUtil.php
```

Closes #27456

COPYBARA_INTEGRATE_REVIEW=https://github.com/protocolbuffers/protobuf/pull/27456 from bshaffer:php-update-reserved-words 522d93f1c159ba71184a7205b648e0d531a8f499
PiperOrigin-RevId: 962903274

php/src/Google/Protobuf/Internal/GPBUtil.php
@@ -250,25 +250,28 @@ class GPBUtil
             return $prefix;
         }
 
+        // BEGIN RESERVED WORDS
+        // @see php/update_reserved_words.sh - DO NOT MODIFY THIS LIST MANUALLY
         $reserved_words = array(
-            "abstract"=>0, "and"=>0, "array"=>0, "as"=>0, "break"=>0,
-            "callable"=>0, "case"=>0, "catch"=>0, "class"=>0, "clone"=>0,
-            "const"=>0, "continue"=>0, "declare"=>0, "default"=>0, "die"=>0,
-            "do"=>0, "echo"=>0, "else"=>0, "elseif"=>0, "empty"=>0,
-            "enddeclare"=>0, "endfor"=>0, "endforeach"=>0, "endif"=>0,
-            "endswitch"=>0, "endwhile"=>0, "eval"=>0, "exit"=>0, "extends"=>0,
-            "final"=>0, "finally"=>0, "fn"=>0, "for"=>0, "foreach"=>0,
-            "function"=>0, "global"=>0, "goto"=>0, "if"=>0, "implements"=>0,
-            "include"=>0, "include_once"=>0, "instanceof"=>0, "insteadof"=>0,
-            "interface"=>0, "isset"=>0, "list"=>0, "match"=>0, "namespace"=>0,
-            "new"=>0, "or"=>0, "parent"=>0, "print"=>0, "private"=>0,
-            "protected"=>0,"public"=>0, "readonly" => 0,"require"=>0,
-            "require_once"=>0,"return"=>0, "self"=>0, "static"=>0, "switch"=>0,
-            "throw"=>0,"trait"=>0, "try"=>0,"unset"=>0, "use"=>0, "var"=>0,
-            "while"=>0,"xor"=>0, "yield"=>0, "int"=>0, "float"=>0, "bool"=>0,
-            "string"=>0,"true"=>0, "false"=>0, "null"=>0, "void"=>0,
-            "iterable"=>0,"object"=>0,"mixed"=>0,"never"=>0
+            "abstract"=>0,     "and"=>0,          "array"=>0,        "as"=>0,           "bool"=>0,
+            "break"=>0,        "callable"=>0,     "case"=>0,         "catch"=>0,        "class"=>0,
+            "clone"=>0,        "const"=>0,        "continue"=>0,     "declare"=>0,      "default"=>0,
+            "die"=>0,          "do"=>0,           "echo"=>0,         "else"=>0,         "elseif"=>0,
+            "empty"=>0,        "enddeclare"=>0,   "endfor"=>0,       "endforeach"=>0,   "endif"=>0,
+            "endswitch"=>0,    "endwhile"=>0,     "eval"=>0,         "exit"=>0,         "extends"=>0,
+            "false"=>0,        "final"=>0,        "finally"=>0,      "float"=>0,        "fn"=>0,
+            "for"=>0,          "foreach"=>0,      "function"=>0,     "global"=>0,       "goto"=>0,
+            "if"=>0,           "implements"=>0,   "include"=>0,      "include_once"=>0, "instanceof"=>0,
+            "insteadof"=>0,    "int"=>0,          "interface"=>0,    "isset"=>0,        "iterable"=>0,
+            "list"=>0,         "match"=>0,        "mixed"=>0,        "namespace"=>0,    "never"=>0,
+            "new"=>0,          "null"=>0,         "object"=>0,       "or"=>0,           "parent"=>0,
+            "print"=>0,        "private"=>0,      "protected"=>0,    "public"=>0,       "readonly"=>0,
+            "require"=>0,      "require_once"=>0, "return"=>0,       "self"=>0,         "static"=>0,
+            "string"=>0,       "switch"=>0,       "throw"=>0,        "trait"=>0,        "true"=>0,
+            "try"=>0,          "unset"=>0,        "use"=>0,          "var"=>0,          "void"=>0,
+            "while"=>0,        "xor"=>0,          "yield"=>0
         );
+        // END RESERVED WORDS
 
         if (array_key_exists(strtolower($classname), $reserved_words)) {
             if ($file_proto->getPackage() === "google.protobuf") {

php/update_reserved_words.sh
@@ -1,0 +1,258 @@
+#!/bin/bash
+
+# The single source of truth for reserved words, categorized by PHP documentation
+# Taken from https://www.php.net/manual/en/reserved.keywords.php
+PHP_RESERVED_KEYWORDS=(
+    "abstract" "and" "array" "as" "break" "callable" "case" "catch"
+    "class" "clone" "const" "continue" "declare" "default" "die" "do"
+    "echo" "else" "elseif" "empty" "enddeclare" "endfor" "endforeach"
+    "endif" "endswitch" "endwhile" "eval" "exit" "extends" "final"
+    "finally" "fn" "for" "foreach" "function" "global" "goto" "if"
+    "implements" "include" "include_once" "instanceof" "insteadof"
+    "interface" "isset" "list" "match" "namespace" "new" "or" "parent"
+    "print" "private" "protected" "public" "readonly" "require"
+    "require_once" "return" "self" "static" "switch" "throw" "trait"
+    "try" "unset" "use" "var" "while" "xor" "yield"
+)
+
+# Taken from https://www.php.net/manual/en/reserved.other-reserved-words.php
+PHP_OTHER_RESERVED_WORDS=(
+    "parent" "self" "int" "float" "bool" "string" "true" "false" "null"
+    "void" "iterable" "object" "mixed" "never" "array" "callable"
+)
+
+# Derived lists:
+# RESERVED_WORDS is the union of keywords and soft reserved words.
+RESERVED_WORDS=($(echo "${PHP_RESERVED_KEYWORDS[@]}" "${PHP_OTHER_RESERVED_WORDS[@]}" | tr ' ' '\n' | sort -u))
+
+# This is for backwards compatibility. New PHP_RESERVED_KEYWORDS should be added
+# here to prevent unnecessary breaking changes, as constants do not need the PB prefix.
+# @TODO: allow ALL names for constants except for "class"
+VALID_CONSTANT_NAMES=(
+    "bool" "false" "float" "int" "iterable" "mixed" "never" "null"
+    "object" "parent" "readonly" "self" "string" "true" "void"
+)
+VALID_CONSTANT_NAMES=($(echo "${VALID_CONSTANT_NAMES[@]}" | tr ' ' '\n' | sort -u))
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPO_ROOT="$(dirname "$SCRIPT_DIR")"
+
+format_columns() {
+    local per_line=$1
+    local indent=$2
+    shift 2
+    local words=("$@")
+    local num_words=${#words[@]}
+
+    # Default to original behavior of quoting the word.
+    local item_fmt="${ITEM_FMT:-\"%s\"}"
+    if [[ -z "$ITEM_FMT" ]]; then
+        item_fmt="\"%s\""
+    fi
+
+    local max_len=0
+    for w in "${words[@]}"; do
+        local s
+        printf -v s "$item_fmt" "$w"
+        if (( ${#s} > max_len )); then max_len=${#s}; fi
+    done
+    local col_width=$((max_len + 2))
+
+    local i=0
+    while (( i < num_words )); do
+        local line="$indent"
+        local j=0
+        while (( j < per_line && i < num_words )); do
+            local w="${words[i]}"
+            local s
+            printf -v s "$item_fmt" "$w"
+            if (( i < num_words - 1 )); then
+                s+=","
+            fi
+
+            if (( j < per_line - 1 && i < num_words - 1 )); then
+                printf -v padded "%-${col_width}s" "$s"
+                line+="$padded"
+            else
+                line+="$s"
+            fi
+
+            i=$i+1
+            j=$j+1
+        done
+        echo "${line%" "}"
+    done
+}
+
+update_file() {
+    local path="$1"
+    local label="$2"
+    local new_content="$3"
+
+    local begin="// BEGIN $label"
+    local end="// END $label"
+    local full_path="$REPO_ROOT/$path"
+    if [[ ! -f "$full_path" ]]; then
+        echo "Skipping $path (not found)"
+        return
+    fi
+
+    # Extract indentation of the first line of content
+    local indent=$(echo "$new_content" | head -n 1 | grep -o '^[[:space:]]*')
+
+    local temp_file=$(mktemp)
+
+    # Use perl to replace the block, writing to a temporary file
+    BEGIN_MARKER="$begin" END_MARKER="$end" NEW_CONTENT="$new_content" INDENT="$indent" \
+    perl -0777 -pe 's{^[ \t]*\Q$ENV{BEGIN_MARKER}\E.*?^[ \t]*\Q$ENV{END_MARKER}\E}{$ENV{INDENT}$ENV{BEGIN_MARKER}\n$ENV{NEW_CONTENT}\n$ENV{INDENT}$ENV{END_MARKER}}sm' "$full_path" > "$temp_file"
+
+    # Move the temporary file to the original file's path
+    mv "$temp_file" "$full_path"
+
+    echo "Updated $path"
+}
+
+generate_names_cc() {
+    local lines=()
+    lines+=("// @see php/update_reserved_words.sh - DO NOT MODIFY THIS LIST MANUALLY")
+    lines+=("const char* const kReservedNames[] = {")
+    local col_lines=()
+    while IFS= read -r line; do
+        col_lines+=("$line")
+    done < <(format_columns 5 "    " "${RESERVED_WORDS[@]}")
+    for l in "${col_lines[@]}"; do lines+=("$l"); done
+    lines[${#lines[@]}-1]+="};"
+    lines+=("const int kReservedNamesSize = ${#RESERVED_WORDS[@]};")
+    printf "%s\n" "${lines[@]}"
+}
+
+generate_names_c() {
+    local lines=()
+    lines+=("// @see php/update_reserved_words.sh - DO NOT MODIFY THIS LIST MANUALLY")
+    lines+=("const char* const kReservedNames[] = {")
+    local decorated=()
+    for w in "${RESERVED_WORDS[@]}"; do
+        decorated+=("\"$w\"")
+    done
+    decorated+=("NULL")
+    local col_lines=()
+    while IFS= read -r line; do
+        col_lines+=("$line")
+    done < <(ITEM_FMT="%s" format_columns 5 "    " "${decorated[@]}")
+    for l in "${col_lines[@]}"; do lines+=("$l"); done
+    lines[${#lines[@]}-1]+="};"
+    printf "%s\n" "${lines[@]}"
+}
+
+generate_php_generator_cc() {
+    local lines=()
+    lines+=("// @see php/update_reserved_words.sh - DO NOT MODIFY THIS LIST MANUALLY")
+    lines+=("constexpr absl::string_view kValidConstantNames[] = {")
+    local col_lines=()
+    while IFS= read -r line; do
+        col_lines+=("$line")
+    done < <(format_columns 6 "    " "${VALID_CONSTANT_NAMES[@]}")
+    for l in "${col_lines[@]}"; do lines+=("$l"); done
+    lines[${#lines[@]}-1]+="};"
+    lines+=("const int kValidConstantNamesSize = ${#VALID_CONSTANT_NAMES[@]};")
+    printf "%s\n" "${lines[@]}"
+}
+
+generate_proto_messages() {
+    local upper=$1
+    echo "// @see php/update_reserved_words.sh - DO NOT MODIFY THIS LIST MANUALLY"
+    for w in "${RESERVED_WORDS[@]}"; do
+        local name="$w"
+        if [[ "$upper" == "true" ]]; then name=$(echo "$w" | tr '[:lower:]' '[:upper:]'); fi
+        echo "message $name {}"
+    done
+}
+
+generate_proto_enums() {
+    local upper=$1
+    echo "// @see php/update_reserved_words.sh - DO NOT MODIFY THIS LIST MANUALLY"
+    local i=1
+    for w in "${RESERVED_WORDS[@]}"; do
+        local name="$w"
+        if [[ "$upper" == "true" ]]; then name=$(echo "$w" | tr '[:lower:]' '[:upper:]'); fi
+        echo "enum $name {"
+        echo "  ZERO$i = 0;"
+        echo "}"
+        i=$((i + 1))
+    done
+}
+
+generate_proto_enum_values() {
+    local upper=$1
+    echo "  // @see php/update_reserved_words.sh - DO NOT MODIFY THIS LIST MANUALLY"
+    local i=0
+    for w in "${RESERVED_WORDS[@]}"; do
+        local name="$w"
+        if [[ "$upper" == "true" ]]; then name=$(echo "$w" | tr '[:lower:]' '[:upper:]'); fi
+        echo "  $name = $i;"
+        i=$((i + 1))
+    done
+}
+
+generate_test_cases() {
+    echo "        // @see php/update_reserved_words.sh - DO NOT MODIFY THIS LIST MANUALLY"
+    # Lower messages
+    for w in "${RESERVED_WORDS[@]}"; do
+        echo "        \$m = new \\Lower\\PB$w();"
+    done
+    echo ""
+    # Upper messages
+    for w in "${RESERVED_WORDS[@]}"; do
+        local upper=$(echo "$w" | tr '[:lower:]' '[:upper:]')
+        echo "        \$m = new \\Upper\\PB$upper();"
+    done
+    echo ""
+    # Lower enums
+    for w in "${RESERVED_WORDS[@]}"; do
+        echo "        \$m = new \\Lower_enum\\PB$w();"
+    done
+    echo ""
+    # Upper enums
+    for w in "${RESERVED_WORDS[@]}"; do
+        local upper=$(echo "$w" | tr '[:lower:]' '[:upper:]')
+        echo "        \$m = new \\Upper_enum\\PB$upper();"
+    done
+    echo ""
+    # Lower enum values
+    for w in "${RESERVED_WORDS[@]}"; do
+        local prefix="PB"
+        for v in "${VALID_CONSTANT_NAMES[@]}"; do
+            if [[ "$w" == "$v" ]]; then prefix=""; break; fi
+        done
+        echo "        \$m = \\Lower_enum_value\\NotAllowed::${prefix}${w};"
+    done
+    echo ""
+    # Upper enum values
+    for w in "${RESERVED_WORDS[@]}"; do
+        local prefix="PB"
+        for v in "${VALID_CONSTANT_NAMES[@]}"; do
+            if [[ "$w" == "$v" ]]; then prefix=""; break; fi
+        done
+        local upper=$(echo "$w" | tr '[:lower:]' '[:upper:]')
+        echo "        \$m = \\Upper_enum_value\\NotAllowed::${prefix}${upper};"
+    done
+}
+
+generate_gpbutil_reserved_words() {
+    echo "        // @see php/update_reserved_words.sh - DO NOT MODIFY THIS LIST MANUALLY"
+    echo "        \$reserved_words = array("
+    ITEM_FMT="\"%s\"=>0" format_columns 5 "            " "${RESERVED_WORDS[@]}"
+    echo "        );"
+}
+
+update_file "src/google/protobuf/compiler/php/names.cc" "RESERVED NAMES" "$(generate_names_cc)"
+update_file "php/ext/google/protobuf/names.c" "RESERVED NAMES" "$(generate_names_c)"
+update_file "src/google/protobuf/compiler/php/php_generator.cc" "VALID CONSTANT NAMES" "$(generate_php_generator_cc)"
+update_file "php/tests/proto/test_reserved_message_lower.proto" "RESERVED NAMES" "$(generate_proto_messages false)"
+update_file "php/tests/proto/test_reserved_message_upper.proto" "RESERVED NAMES" "$(generate_proto_messages true)"
+update_file "php/tests/proto/test_reserved_enum_lower.proto" "RESERVED NAMES" "$(generate_proto_enums false)"
+update_file "php/tests/proto/test_reserved_enum_upper.proto" "RESERVED NAMES" "$(generate_proto_enums true)"
+update_file "php/tests/proto/test_reserved_enum_value_lower.proto" "RESERVED NAMES" "$(generate_proto_enum_values false)"
+update_file "php/tests/proto/test_reserved_enum_value_upper.proto" "RESERVED NAMES" "$(generate_proto_enum_values true)"
+update_file "php/tests/GeneratedClassTest.php" "RESERVED NAMES" "$(generate_test_cases)"
+update_file "php/src/Google/Protobuf/Internal/GPBUtil.php" "RESERVED WORDS" "$(generate_gpbutil_reserved_words)"

php/tests/proto/test_reserved_enum_value_lower.proto
@@ -3,95 +3,98 @@ syntax = "proto3";
 package lower_enum_value;
 
 enum NotAllowed {
+  // BEGIN RESERVED NAMES
+  // @see php/update_reserved_words.sh - DO NOT MODIFY THIS LIST MANUALLY
   abstract = 0;
   and = 1;
   array = 2;
   as = 3;
-  break = 4;
-  callable = 5;
-  case = 6;
-  catch = 7;
-  class = 8;
-  clone = 9;
-  const = 10;
-  continue = 11;
-  declare = 12;
-  default = 13;
-  die = 14;
-  do = 15;
-  echo = 16;
-  else = 17;
-  elseif = 18;
-  empty = 19;
-  enddeclare = 20;
-  endfor = 21;
-  endforeach = 22;
-  endif = 23;
-  endswitch = 24;
-  endwhile = 25;
-  eval = 26;
-  exit = 27;
-  extends = 28;
-  final = 29;
-  finally = 30;
-  fn = 31;
+  bool = 4;
+  break = 5;
+  callable = 6;
+  case = 7;
+  catch = 8;
+  class = 9;
+  clone = 10;
+  const = 11;
+  continue = 12;
+  declare = 13;
+  default = 14;
+  die = 15;
+  do = 16;
+  echo = 17;
+  else = 18;
+  elseif = 19;
+  empty = 20;
+  enddeclare = 21;
+  endfor = 22;
+  endforeach = 23;
+  endif = 24;
+  endswitch = 25;
+  endwhile = 26;
+  eval = 27;
+  exit = 28;
+  extends = 29;
+  false = 30;
+  final = 31;
+  finally = 32;
+  float = 33;
+  fn = 34;
   for
-    = 32;
-  foreach = 33;
-  function = 34;
-  global = 35;
-  goto = 36;
+    = 35;
+  foreach = 36;
+  function = 37;
+  global = 38;
+  goto = 39;
   if
-    = 37;
-  implements = 38;
-  include = 39;
-  include_once = 40;
-  instanceof = 41;
-  insteadof = 42;
-  interface = 43;
-  isset = 44;
-  list = 45;
-  match = 46;
-  namespace = 47;
-  new = 48;
-  or = 49;
-  parent = 77;
-  print = 50;
+    = 40;
+  implements = 41;
+  include = 42;
+  include_once = 43;
+  instanceof = 44;
+  insteadof = 45;
+  int = 46;
+  interface = 47;
+  isset = 48;
+  iterable = 49;
+  list = 50;
+  match = 51;
+  mixed = 52;
+  namespace = 53;
+  never = 54;
+  new = 55;
+  null = 56;
+  object = 57;
+  or = 58;
+  parent = 59;
+  print = 60;
  private
-  = 51;
+  = 61;
  protected
-  = 52;
+  = 62;
  public
-  = 53;
-  readonly = 79;
-  require = 54;
-  require_once = 55;
-  return = 56;
-  self = 78;
-  static = 57;
+  = 63;
+  readonly = 64;
+  require = 65;
+  require_once = 66;
+  return = 67;
+  self = 68;
+  static = 69;
+  string = 70;
   switch
-    = 58;
-  throw = 59;
-  trait = 60;
+    = 71;
+  throw = 72;
+  trait = 73;
+  true = 74;
   try
-    = 61;
-  unset = 62;
-  use = 63;
-  var = 64;
+    = 75;
+  unset = 76;
+  use = 77;
+  var = 78;
+  void = 79;
   while
-    = 65;
-  xor = 66;
-  yield = 67;
-  int = 68;
-  float = 69;
-  bool = 70;
-  string = 71;
-  true = 72;
-  false = 73;
-  null = 74;
-  void = 75;
-  iterable = 76;
-  object = 80;
-  mixed = 81;
-  never = 82;
+    = 80;
+  xor = 81;
+  yield = 82;
+  // END RESERVED NAMES
 }

php/tests/proto/test_reserved_message_lower.proto
@@ -2,10 +2,13 @@ syntax = "proto3";
 
 package lower;
 
+// BEGIN RESERVED NAMES
+// @see php/update_reserved_words.sh - DO NOT MODIFY THIS LIST MANUALLY
 message abstract {}
 message and {}
 message array {}
 message as {}
+message bool {}
 message break {}
 message callable {}
 message case {}
@@ -33,8 +36,10 @@ message endwhile {}
 message eval {}
 message exit {}
 message extends {}
+message false {}
 message final {}
 message finally {}
+message float {}
 message fn {}
 message for {}
 message foreach {}
@@ -47,13 +52,19 @@ message include {}
 message include_once {}
 message instanceof {}
 message insteadof {}
+message int {}
 message interface {
 } message isset {
 }
+message iterable {}
 message list {}
 message match {}
+message mixed {}
 message namespace {}
+message never {}
 message new {}
+message null {}
+message object {}
 message or {}
 message parent {}
 message print {}
@@ -66,25 +77,17 @@ message require_once {}
 message return {}
 message self {}
 message static {}
+message string {}
 message switch {}
 message throw {}
 message trait {}
+message true {}
 message try {}
 message unset {}
 message use {}
 message var {}
+message void {}
 message while {}
 message xor {}
 message yield {}
-message int {}
-message float {}
-message bool {}
-message string {}
-message true {}
-message false {}
-message null {}
-message void {}
-message iterable {}
-message object {}
-message mixed {}
-message never {}
+// END RESERVED NAMES

commit 7b03bd43050084e2039935619f59b263a96349f9

Fix PHP update_reserved_words.sh formatting and CI idempotency test
Fix the GitHub Actions CI failure in
`UpdateReservedTest::testBashScriptIdempotency` introduced by PR #27456.

### Problem:

`update_reserved_words.sh` generated array formatting that differed from
the repository's checked-in files, causing the strict idempotency test
to fail:
1. Column padding used a single global max width across all columns
   instead of calculating padding per column.
2. Generated array line lengths in multiple files exceeded 80
   characters.
3. Proto test files had slight formatting differences compared to the
   script's one-liner output.

### Solution:

- `update_reserved_words.sh`:
  * Updated `format_columns()` to calculate padding per-column in pure
    Bash, matching standard C/C++ array alignment without requiring
    external tools.
  * Adjusted array column counts (`names.c` to 4, `php_generator.cc` to
    5, `GPBUtil.php` to 3) so lines stay strictly within 80 characters.
- `GPBUtil.php` & proto test files:
  * Synced formatting to match the updated script output byte-for-byte.

PiperOrigin-RevId: 962935787

php/src/Google/Protobuf/Internal/GPBUtil.php
@@ -253,23 +253,34 @@ class GPBUtil
         // BEGIN RESERVED WORDS
         // @see php/update_reserved_words.sh - DO NOT MODIFY THIS LIST MANUALLY
         $reserved_words = array(
-            "abstract"=>0,     "and"=>0,          "array"=>0,        "as"=>0,           "bool"=>0,
-            "break"=>0,        "callable"=>0,     "case"=>0,         "catch"=>0,        "class"=>0,
-            "clone"=>0,        "const"=>0,        "continue"=>0,     "declare"=>0,      "default"=>0,
-            "die"=>0,          "do"=>0,           "echo"=>0,         "else"=>0,         "elseif"=>0,
-            "empty"=>0,        "enddeclare"=>0,   "endfor"=>0,       "endforeach"=>0,   "endif"=>0,
-            "endswitch"=>0,    "endwhile"=>0,     "eval"=>0,         "exit"=>0,         "extends"=>0,
-            "false"=>0,        "final"=>0,        "finally"=>0,      "float"=>0,        "fn"=>0,
-            "for"=>0,          "foreach"=>0,      "function"=>0,     "global"=>0,       "goto"=>0,
-            "if"=>0,           "implements"=>0,   "include"=>0,      "include_once"=>0, "instanceof"=>0,
-            "insteadof"=>0,    "int"=>0,          "interface"=>0,    "isset"=>0,        "iterable"=>0,
-            "list"=>0,         "match"=>0,        "mixed"=>0,        "namespace"=>0,    "never"=>0,
-            "new"=>0,          "null"=>0,         "object"=>0,       "or"=>0,           "parent"=>0,
-            "print"=>0,        "private"=>0,      "protected"=>0,    "public"=>0,       "readonly"=>0,
-            "require"=>0,      "require_once"=>0, "return"=>0,       "self"=>0,         "static"=>0,
-            "string"=>0,       "switch"=>0,       "throw"=>0,        "trait"=>0,        "true"=>0,
-            "try"=>0,          "unset"=>0,        "use"=>0,          "var"=>0,          "void"=>0,
-            "while"=>0,        "xor"=>0,          "yield"=>0
+            "abstract"=>0,     "and"=>0,          "array"=>0,
+            "as"=>0,           "bool"=>0,         "break"=>0,
+            "callable"=>0,     "case"=>0,         "catch"=>0,
+            "class"=>0,        "clone"=>0,        "const"=>0,
+            "continue"=>0,     "declare"=>0,      "default"=>0,
+            "die"=>0,          "do"=>0,           "echo"=>0,
+            "else"=>0,         "elseif"=>0,       "empty"=>0,
+            "enddeclare"=>0,   "endfor"=>0,       "endforeach"=>0,
+            "endif"=>0,        "endswitch"=>0,    "endwhile"=>0,
+            "eval"=>0,         "exit"=>0,         "extends"=>0,
+            "false"=>0,        "final"=>0,        "finally"=>0,
+            "float"=>0,        "fn"=>0,           "for"=>0,
+            "foreach"=>0,      "function"=>0,     "global"=>0,
+            "goto"=>0,         "if"=>0,           "implements"=>0,
+            "include"=>0,      "include_once"=>0, "instanceof"=>0,
+            "insteadof"=>0,    "int"=>0,          "interface"=>0,
+            "isset"=>0,        "iterable"=>0,     "list"=>0,
+            "match"=>0,        "mixed"=>0,        "namespace"=>0,
+            "never"=>0,        "new"=>0,          "null"=>0,
+            "object"=>0,       "or"=>0,           "parent"=>0,
+            "print"=>0,        "private"=>0,      "protected"=>0,
+            "public"=>0,       "readonly"=>0,     "require"=>0,
+            "require_once"=>0, "return"=>0,       "self"=>0,
+            "static"=>0,       "string"=>0,       "switch"=>0,
+            "throw"=>0,        "trait"=>0,        "true"=>0,
+            "try"=>0,          "unset"=>0,        "use"=>0,
+            "var"=>0,          "void"=>0,         "while"=>0,
+            "xor"=>0,          "yield"=>0
         );
         // END RESERVED WORDS
 

php/update_reserved_words.sh
@@ -50,13 +50,27 @@ format_columns() {
         item_fmt="\"%s\""
     fi
 
-    local max_len=0
-    for w in "${words[@]}"; do
-        local s
-        printf -v s "$item_fmt" "$w"
-        if (( ${#s} > max_len )); then max_len=${#s}; fi
+    # Compute max width per column to match Google C++/C formatting
+    local col_widths=()
+    local c=0
+    while (( c < per_line )); do
+        local max_w=0
+        local k=$c
+        while (( k < num_words )); do
+            local w="${words[k]}"
+            local s
+            printf -v s "$item_fmt" "$w"
+            if (( k < num_words - 1 )); then
+                s+=","
+            fi
+            if (( ${#s} > max_w )); then
+                max_w=${#s}
+            fi
+            k=$((k + per_line))
+        done
+        col_widths+=($((max_w + 1)))
+        c=$((c + 1))
     done
-    local col_width=$((max_len + 2))
 
     local i=0
     while (( i < num_words )); do
@@ -71,14 +85,15 @@ format_columns() {
             fi
 
             if (( j < per_line - 1 && i < num_words - 1 )); then
-                printf -v padded "%-${col_width}s" "$s"
+                local cw="${col_widths[j]}"
+                printf -v padded "%-${cw}s" "$s"
                 line+="$padded"
             else
                 line+="$s"
             fi
 
-            i=$i+1
-            j=$j+1
+            i=$((i + 1))
+            j=$((j + 1))
         done
         echo "${line%" "}"
     done
@@ -138,7 +153,7 @@ generate_names_c() {
     local col_lines=()
     while IFS= read -r line; do
         col_lines+=("$line")
-    done < <(ITEM_FMT="%s" format_columns 5 "    " "${decorated[@]}")
+    done < <(ITEM_FMT="%s" format_columns 4 "    " "${decorated[@]}")
     for l in "${col_lines[@]}"; do lines+=("$l"); done
     lines[${#lines[@]}-1]+="};"
     printf "%s\n" "${lines[@]}"
@@ -151,7 +166,7 @@ generate_php_generator_cc() {
     local col_lines=()
     while IFS= read -r line; do
         col_lines+=("$line")
-    done < <(format_columns 6 "    " "${VALID_CONSTANT_NAMES[@]}")
+    done < <(format_columns 5 "    " "${VALID_CONSTANT_NAMES[@]}")
     for l in "${col_lines[@]}"; do lines+=("$l"); done
     lines[${#lines[@]}-1]+="};"
     lines+=("const int kValidConstantNamesSize = ${#VALID_CONSTANT_NAMES[@]};")
@@ -241,7 +256,7 @@ generate_test_cases() {
 generate_gpbutil_reserved_words() {
     echo "        // @see php/update_reserved_words.sh - DO NOT MODIFY THIS LIST MANUALLY"
     echo "        \$reserved_words = array("
-    ITEM_FMT="\"%s\"=>0" format_columns 5 "            " "${RESERVED_WORDS[@]}"
+    ITEM_FMT="\"%s\"=>0" format_columns 3 "            " "${RESERVED_WORDS[@]}"
     echo "        );"
 }
 

php/tests/proto/test_reserved_enum_value_lower.proto
@@ -40,14 +40,12 @@ enum NotAllowed {
   finally = 32;
   float = 33;
   fn = 34;
-  for
-    = 35;
+  for = 35;
   foreach = 36;
   function = 37;
   global = 38;
   goto = 39;
-  if
-    = 40;
+  if = 40;
   implements = 41;
   include = 42;
   include_once = 43;
@@ -68,12 +66,9 @@ enum NotAllowed {
   or = 58;
   parent = 59;
   print = 60;
- private
-  = 61;
- protected
-  = 62;
- public
-  = 63;
+  private = 61;
+  protected = 62;
+  public = 63;
   readonly = 64;
   require = 65;
   require_once = 66;
@@ -81,19 +76,16 @@ enum NotAllowed {
   self = 68;
   static = 69;
   string = 70;
-  switch
-    = 71;
+  switch = 71;
   throw = 72;
   trait = 73;
   true = 74;
-  try
-    = 75;
+  try = 75;
   unset = 76;
   use = 77;
   var = 78;
   void = 79;
-  while
-    = 80;
+  while = 80;
   xor = 81;
   yield = 82;
   // END RESERVED NAMES

php/tests/proto/test_reserved_message_lower.proto
@@ -13,9 +13,8 @@ message break {}
 message callable {}
 message case {}
 message catch {}
-message class {
-} message clone {
-}
+message class {}
+message clone {}
 message const {}
 message continue {}
 message declare {}
@@ -23,8 +22,7 @@ message default {}
 message die {}
 message do {}
 message echo {}
-message else {
-}
+message else {}
 message elseif {}
 message empty {}
 message enddeclare {}
@@ -53,9 +51,8 @@ message include_once {}
 message instanceof {}
 message insteadof {}
 message int {}
-message interface {
-} message isset {
-}
+message interface {}
+message isset {}
 message iterable {}
 message list {}
 message match {}